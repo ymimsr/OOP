339cc759971ce41d301092292026f06c5b955893
ymimsr <ymimsr@gmail.com>
2021-12-24 11:09:18 +0700

    add tests

--- Task_1_2_2/src/main/java/MyTree.java
+++ Task_1_2_2/src/main/java/MyTree.java
@@ -3,33 +3,63 @@ import java.util.*;
 
 public class MyTree<E> implements Collection<E> {
 
-    private E data;
-    private MyTree<E> ancestor;
-    private List<MyTree<E>> descendants;
-    private int size;
+    private Node<E> root;
+    private int size = 0;
 
     public MyTree(E data) {
-        this.data = data;
-        this.ancestor = null;
-        size = 1;
+        this.root = new Node<>(data);
+        this.size = 1;
     }
 
-    public MyTree<E> addElem(E elem, MyTree<E> node) {
-        MyTree<E> newNode = new MyTree<>(elem);
-        newNode.ancestor = node;
-        node.descendants.add(newNode);
-        size++;
-
-        return newNode;
+    public MyTree() {
     }
 
-    public MyTree<E> addElem(E elem) {
-        MyTree<E> newNode = new MyTree<>(elem);
-        newNode.ancestor = this;
-        descendants.add(newNode);
-        size++;
+    public Node<E> getRoot() {
+        return root;
+    }
 
-        return newNode;
+    public static class Node<E> {
+        private final E data;
+        private Node<E> ancestor;
+        private final List<Node<E>> descendants = new ArrayList<>();
+
+        private Node(E data) {
+            this.data = data;
+        }
+
+        public E getData() {
+            return data;
+        }
+    }
+
+    public Node<E> addElem(Node<E> ancestor, E elem) {
+        Node<E> node = new Node<>(elem);
+        node.ancestor = ancestor;
+        if (root == null) {
+            if (ancestor == null) {
+                root = node;
+            } else {
+                throw new NoSuchElementException("Given node is not in a tree");
+            }
+        } else {
+            ancestor.descendants.add(node);
+        }
+
+        size++;
+        return node;
+    }
+
+    public Node<E> addElem(E elem) {
+        Node<E> node = new Node<>(elem);
+        node.ancestor = root;
+        if (root == null) {
+            root = node;
+        } else {
+            root.descendants.add(node);
+        }
+
+        size++;
+        return node;
     }
 
     @Override
@@ -58,86 +88,29 @@ public class MyTree<E> implements Collection<E> {
     @Override
     public Iterator<E> iterator() {
         return new Iterator<>() {
-            // index is depth of the tree, the depth <= size of tree
-            // value is index in the descendents list
-            //private int[] descIndex = new int[size];
-            private int iterIndex = 0;
-            private int descIndex = 0;
+
+            private final Stack<Node<E>> stack = new Stack<>();
+
+            {
+                stack.push(root);
+            }
 
             @Override
             public boolean hasNext() {
-                return iterIndex < size;
+                return !stack.isEmpty();
             }
 
             @Override
             public E next() {
-                if (descendants.size() != 0) {
-                    return descendants.get(descIndex++).iterator().next();
+                if (!stack.isEmpty()) {
+                    Node<E> curNode = stack.pop();
+                    for (int i = curNode.descendants.size() - 1; i >= 0; i--) {
+                        stack.push(curNode.descendants.get(i));
+                    }
+                    return curNode.data;
                 } else {
-                    iterIndex++;
-                    return data;
+                    throw new NoSuchElementException("There are no more elements of a tree");
                 }
-            }
-
-        };
-    }
-
-    public ListIterator<E> listIterator() {
-        return new ListIterator<>() {
-
-            private int iterIndex = 0;
-            private int descIndex = 0;
-
-            @Override
-            public boolean hasNext() {
-                return iterIndex < size;
-            }
-
-            @Override
-            public E next() {
-                if (descendants.size() != 0) {
-                    return descendants.get(descIndex++).iterator().next();
-                } else {
-                    iterIndex++;
-                    return data;
-                }
-            }
-
-            @Override
-            public boolean hasPrevious() {
-                return false;
-            }
-
-            @Override
-            public E previous() {
-                return null;
-            }
-
-            @Override
-            public int nextIndex() {
-                return 0;
-            }
-
-            @Override
-            public int previousIndex() {
-                return 0;
-            }
-
-            @Override
-            public void remove() {
-                if (MyTree.this.ancestor != null) {
-                    MyTree.this.ancestor.descendants.remove(descIndex);
-                }
-            }
-
-            @Override
-            public void set(E e) {
-
-            }
-
-            @Override
-            public void add(E e) {
-
             }
 
         };
@@ -182,34 +155,86 @@ public class MyTree<E> implements Collection<E> {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public boolean remove(Object o) {
+        E obj = (E) o;
+        // if we need to remove the root node
+        // we will make the leftmost descendent of root the new root
+        if (root.data.equals(obj)) {
+            if (root.descendants.size() != 0) {
+                for (int i = 1; i < root.descendants.size(); i++) {
+                    root.descendants.get(0).descendants.add(root.descendants.get(i));
+                }
+                root.descendants.get(0).ancestor = null;
+                root = root.descendants.get(0);
+                size--;
+            } else {
+                root = null;
+                size = 0;
+            }
+            return true;
+        }
+
+        Node<E> curNode = root;
+        for (int i = 0; i < size;) {
+            for (int k = 0; k < curNode.descendants.size(); k++) {
+                Node<E> descendant = curNode.descendants.get(k);
+                if (descendant.data.equals(obj)) {
+                    curNode.descendants.addAll(k, descendant.descendants);
+                    descendant.descendants.forEach(eNode -> eNode.ancestor = curNode);
+                    curNode.descendants.remove(k);
+                    return true;
+                }
+                i++;
+            }
+        }
 
         return false;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        for (Object o : c) {
+            if (!contains(o)) return false;
+        }
+
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        for (E e : c) {
+            add(e);
+        }
+
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        boolean isChanged = false;
+        for (Object o : c) {
+            if (remove(o)) isChanged = true;
+        }
+        return isChanged;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        boolean isChanged = false;
+        for (E e : this) {
+            if (!c.contains(e)) {
+                remove(e);
+                isChanged = true;
+            }
+        }
+        return isChanged;
     }
 
     @Override
     public void clear() {
         // garbage collector will do all the work
-        descendants = null;
+        root = null;
+        size = 0;
     }
 }
