34c281e9a1ec36997f6d8b5e7dc432b4727d2234
ymim <43496855+ymimsr@users.noreply.github.com>
2022-05-18 12:17:03 +0700

    change snakes color

--- Task_2_3_1/src/main/java/ru/nsu/fit/oop/task_2_3_1/view/CanvasPainter.java
+++ Task_2_3_1/src/main/java/ru/nsu/fit/oop/task_2_3_1/view/CanvasPainter.java
@@ -14,8 +14,8 @@ public class CanvasPainter implements Painter {
     private static final Color borderCanvasColor = Color.GREEN;
     private static final Color foodColor = Color.YELLOW;
     private static final Color obstacleColor = Color.RED;
-    private static final Color selfSnakeColor = Color.CORAL;
-    private static final Color snakeColor = Color.GREEN;
+    private static final Color selfSnakeColor = Color.GREEN;
+    private static final Color snakeColor = Color.CORAL;
 
     private static final int sizeX = 600;
     private static final int sizeY = 600;
@@ -51,65 +51,68 @@ public class CanvasPainter implements Painter {
 
         calcTileSize(field);
 
-        gc.setFill(borderCanvasColor);
+        // offset
+        double xt = 0;
+        double yt = 0;
         if (tileSize * field.getSizeX() > tileSize * field.getSizeY()) {
-            double t = (tileSize * field.getSizeX() - tileSize * field.getSizeY()) / 2;
-            gc.fillRect(0, t - borderWidth, sizeX + borderWidth, sizeY - 2 * t + borderWidth);
+            yt = (tileSize * field.getSizeX() - tileSize * field.getSizeY()) / 2;
         } else {
-            double t = (tileSize * field.getSizeY() - tileSize * field.getSizeX()) / 2;
-            gc.fillRect(t - borderWidth, 0, sizeX - 2 * t + borderWidth, sizeY + borderWidth);
+            xt = (tileSize * field.getSizeY() - tileSize * field.getSizeX()) / 2;
         }
 
+        gc.setFill(borderCanvasColor);
+        gc.fillRect(xt - borderWidth, yt - borderWidth, sizeX - 2 * xt + 2 * borderWidth, sizeY - 2 * yt + 2 * borderWidth);
+
         gc.setFill(freeTileColor);
-        gc.fillRect(0, 0, sizeX, sizeY);
+        gc.fillRect(xt, yt, sizeX - 2 * xt, sizeY - 2 * yt);
 
         gc.setFill(foodColor);
         for (Food food : field.getFoods()) {
             int x = food.getTile().getPosition().x();
             int y = food.getTile().getPosition().y();
-            gc.fillRect(tileSize * x, tileSize * y, tileSize, tileSize);
+            gc.fillRect(tileSize * x + xt, tileSize * y + yt, tileSize, tileSize);
         }
 
         gc.setFill(obstacleColor);
         for (Obstacle obstacle : field.getObstacles()) {
             int x = obstacle.getTile().getPosition().x();
             int y = obstacle.getTile().getPosition().y();
-            gc.fillRect(tileSize * x, tileSize * y, tileSize, tileSize);
+            gc.fillRect(tileSize * x + xt, tileSize * y + yt, tileSize, tileSize);
         }
 
-        paintSnake(selfSnakeColor, game.getSelfSnake());
+        paintSnake(selfSnakeColor, game.getSelfSnake(), xt, yt);
         for (Snake snake : snakes) {
             if (snake != game.getSelfSnake())
-                paintSnake(snakeColor, snake);
+                paintSnake(snakeColor, snake, xt, yt);
         }
 
     }
 
-    private void paintSnake(Color color, Snake snake) {
+    private void paintSnake(Color color, Snake snake, double xt, double yt) {
         for (SnakePart snakePart : snake.getSnakeBody()) {
             gc.setFill(color);
             int x = snakePart.getTile().getPosition().x();
             int y = snakePart.getTile().getPosition().y();
-            gc.fillRect(tileSize * x, tileSize * y, tileSize, tileSize);
+            gc.fillRect(tileSize * x + xt, tileSize * y + yt, tileSize, tileSize);
             if (snakePart.isHead()) {
                 // drawing eyes
                 gc.setFill(Color.RED);
                 switch (snake.getDirection()) {
                     case UP -> {
-                        gc.fillRect(tileSize * x + tileSize / 4, tileSize * y + 2 * tileSize / 3, 2, 2);
-                        gc.fillRect(tileSize * x + 3 * tileSize / 4, tileSize * y + 2 * tileSize / 3, 2, 2);
+                        gc.fillRect(tileSize * x + tileSize / 4 + xt, tileSize * y + 2 * tileSize / 3 + yt, 2, 2);
+                        gc.fillRect(tileSize * x + 3 * tileSize / 4 + xt, tileSize * y + 2 * tileSize / 3 + yt, 2, 2);
                     }
                     case DOWN -> {
-                        gc.fillRect(tileSize * x + tileSize / 4, tileSize * y + tileSize / 3, 2, 2);
-                        gc.fillRect(tileSize * x + 3 * tileSize / 4, tileSize * y + tileSize / 3, 2, 2);
+                        gc.fillRect(tileSize * x + tileSize / 4 + xt, tileSize * y + tileSize / 3 + yt, 2, 2);
+                        gc.fillRect(tileSize * x + 3 * tileSize / 4 + xt, tileSize * y + tileSize / 3 + yt, 2, 2);
                     }
                     case RIGHT -> {
-                        gc.fillRect(tileSize * x + 2 * tileSize / 3, tileSize * y + tileSize / 4, 2, 2);
-                        gc.fillRect(tileSize * x + 2 * tileSize / 3, tileSize * y + 3 * tileSize / 4, 2, 2);
+                        gc.fillRect(tileSize * x + 2 * tileSize / 3 + xt, tileSize * y + tileSize / 4 + yt, 2, 2);
+                        gc.fillRect(tileSize * x + 2 * tileSize / 3 + xt, tileSize * y + 3 * tileSize / 4 + yt, 2, 2);
                     }
                     case LEFT -> {
-                        gc.fillRect(tileSize * x + tileSize / 3, tileSize * y + tileSize / 4, 2, 2);
-                        gc.fillRect(tileSize * x + tileSize / 3, tileSize * y + 3 * tileSize / 4, 2, 2);
+                        gc.fillRect(tileSize * x + tileSize / 3 + xt, tileSize * y + tileSize / 4 + yt, 2, 2);
+                        gc.fillRect(tileSize * x + tileSize / 3 + xt, tileSize * y + 3 * tileSize / 4 + yt, 2, 2);
                     }
                 }
             }
